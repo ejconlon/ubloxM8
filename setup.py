589fc423adcde0d403c0b6c10522266df99ae9e4
Eric Conlon <ejconlon@gmail.com>
2016-04-12 13:00:45 -0700

    installation script

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name="ublox_m8",
   ext_modules=[
     Extension("ublox_m8", sources = ["src/ublox_m8_python.cpp",
                                      "src/ublox_m8.cpp"],
-    libraries = ["boost_python-py27", "boost_thread", "serial"],
+    libraries = ["boost_python-mt", "boost_thread-mt", "serial"],
     include_dirs = ["include/"],
     extra_compile_args=['-std=c++11'])
   ])
